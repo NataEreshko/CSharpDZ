9f715e82ad48984fd4c4cf5d72e77c13f648eb6d
Nataly <nataereshko@gmail.com>
2022-12-13 03:17:52 +0500

    работа над ошибками

--- zadanie25/Program.cs
+++ zadanie25/Program.cs
@@ -1,23 +1,29 @@
 ﻿// Напишите цикл(программу), который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-Console.Write($"Введите число A: ");
-int A;
-int.TryParse(Console.ReadLine(), out A);
-
-Console.Write($"Введите число B: ");
-int B;
-int.TryParse(Console.ReadLine(), out B);
-
-if (B <= 0){
-    Console.WriteLine($"Неверное число B");
-    return 1;
-
+int fillVar(string var)
+{
+    int V;
+    Console.Write($"Введите число {var}: ");
+    int.TryParse(Console.ReadLine(), out V);
+    return V;
 }
 
-int result = 1;
+int calc(int a, int b)
+{
+    int result = 1;
 
-for (int i = 0; i < B; i++) 
-    result *= A;
+    for (int i = 0; i < b; i++) 
+        result *= a;
+    return result;
+}
 
-Console.WriteLine($" {A} ^ {B} = {result} ");
+int A = fillVar("A");
+int B = fillVar("B");
+
+if (B <= 0) {
+    Console.WriteLine($"Неверное число B");
+    return 1;
+}
+
+Console.WriteLine($" {A} ^ {B} = {calc(A, B)} ");
 
 return 0;
--- zadanie27/Program.cs
+++ zadanie27/Program.cs
@@ -1,12 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int calc(string? num)
+{
+    int sum = 0;
+    if (num == null)
+        return sum;
+    
+    for (int i = 0; i < num.Length; i++)
+        sum += int.Parse(num[i].ToString());    
+    return sum;
+}
+
 Console.Write("Введите число: ");
 string? num = Console.ReadLine();
-if (num == null)
-    return 1;
 
-int sum = 0;
-for (int i = 0; i < num.Length; i++)
-    sum += int.Parse(num[i].ToString());
-
-Console.WriteLine($"{num} -> {sum}");
-return 0;
+Console.WriteLine($"{num} -> {calc(num)}");
